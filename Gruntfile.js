cf048a9af2378c4416324884b96e4836ec463665
Haralambos <hgogonis@gmail.com>
2015-08-23 22:23:00 -0400

    added build

--- Gruntfile.js
+++ Gruntfile.js
@@ -258,7 +258,7 @@ module.exports = function (grunt) {
         flow: {
           html: {
             steps: {
-              js: ['concat', 'uglifyjs'],
+              js: ['concat'],
               css: ['cssmin']
             },
             post: {}
