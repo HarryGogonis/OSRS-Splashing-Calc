22c8a7798a4a7c1a98dee3f92fbb4886577dc9cc
Haralambos <hgogonis@gmail.com>
2015-08-29 15:33:32 -0400

    fixed problem with build script and added new build

--- Gruntfile.js
+++ Gruntfile.js
@@ -258,7 +258,7 @@ module.exports = function (grunt) {
         flow: {
           html: {
             steps: {
-              js: ['concat'],
+              js: ['concat', 'uglifyjs'],
               css: ['cssmin']
             },
             post: {}
